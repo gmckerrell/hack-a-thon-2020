627c1994a7cd03a774414c92f36d3f9fd9983587
Andy Juniper <andy_juniper@mcafee.com>
2020-05-15 12:34:16 +0100

    move code inline, just save zip file in given dir
    can also specify date too

--- fs/app/get_dns.py
+++ fs/app/get_dns.py
@@ -16,37 +16,52 @@
 
 import sys, getopt
 from datetime import datetime, timedelta
-import hnrd
+import base64
+import requests
 
 def main(argv):
-    dowloaded_file_path = ''
+    downloaded_file_path = ''
+    # default the date to download to yesterday which will be the most recent
+    date=datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
     try:
-        opts, args = getopt.getopt(argv,"hd:")
+        opts, args = getopt.getopt(argv,"hd:D:")
     except getopt.GetoptError:
-        print ('get_dns.py -d <dowloaded_file_path>')
+        print ('get_dns.py -d <dowloaded_file_path> [-D YYYY-MM-DD]')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('get_dns.py -d <dowloaded_file_path>')
+            print ('get_dns.py -d <dowloaded_file_path> [-D YYYY-MM-DD]')
             sys.exit()
         elif opt == ("-d"):
-            dowloaded_file_path = arg
+            downloaded_file_path = arg
+        elif opt == "-D":
+            date = arg
 
-    print ('downloaded file path is ', dowloaded_file_path)
+    print ('downloaded file path is ', downloaded_file_path)
 
-    download(dowloaded_file_path)
+    download(downloaded_file_path,date)
 
     print ("DOWNLOADED")
 
-def download(dowloaded_file_path):
-    yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+def download(downloaded_file_path,d):
     
-    try:
-        hnrd.donwnload_nrd(yesterday)
-    except:
-        print("Not a correct input (example: 2010-10-10)")
-        sys.exit()
+    outfile=downloaded_file_path + "/newly-registered-domains-"+d+".zip"
+    print(outfile)
 
+    b64 = base64.b64encode((d+".zip").encode('ascii'))
+    nrd_zip = 'https://whoisds.com//whois-database/newly-registered-domains/{}/nrd'.format(b64.decode('ascii'))
+    try:
+        resp = requests.get(nrd_zip,stream=True)
+
+        print("Downloading File {} - Size {}...".format(d+'.zip',resp.headers['Content-length']))
+        if resp.headers['Content-length']:
+            with open(outfile, 'wb') as f:
+                for data in resp.iter_content(chunk_size=1024):
+                    f.write(data)
+    except Exception as exc:
+        print(('exception: %s' % (exc)),file=sys.stderr)
+        print("File {}.zip does not exist on the remote server.".format(d))
+        sys.exit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
