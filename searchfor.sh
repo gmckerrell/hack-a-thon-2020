dc9aaaf1a07be56ba8db5a528e9645d3fe1287d1
Andy Juniper <andy_juniper@mcafee.com>
2020-05-15 11:47:03 +0100

    cache the processed source files
    cache the processed source files as git doesn't preserve timestamps

--- searchfor.sh
+++ searchfor.sh
@@ -16,15 +16,18 @@ searchhash=$(base64 -w 0 <<<"$search")
 
 results=$outdir/$searchhash
 if [[ -e $results ]] ; then
-    findargs="-and -newer $results"
+    findargs="grep -v -F -f $results.processed"
+else
+    findargs=cat
 fi
 
 mydir=$(dirname $(readlink -f $0))
 
-# find the new domain files which are newer
-find $indir -type f $findargs | while read f ; do
+# find the new domain files which we have not seen before
+find $indir -type f | $findargs | while read f ; do
     # for each newer file, crawl the results
     python ${mydir}/hnrd.py -n -f "$f" -s "$search"
+    echo "$f" >>$results.processed
 done >>$results
 
-# TODO push $results to github preserving timestamp
+# TODO push $results and $results.processed back to repo
