a6201fa2dba012ed54810ab32a01c254a99e4c24
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-05-18 13:00:23 +0100

    Create commit_and_push.sh

---
+++ commit_and_push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+COMMENT=$1; shift
+git config --local user.name "$GITHUB_ACTOR"
+git config --local user.email "$GITHUB_ACTOR@bots.github.com"
+git add -v "${@}"
+git commit -m "$COMMENT" -a || exit 0
+git pull
+git push
