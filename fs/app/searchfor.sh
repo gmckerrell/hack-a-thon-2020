3b3b8d9697b9810d2f9635107c90e59b57ccf197
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-05-19 10:14:03 +0100

    Update searchfor.sh

--- fs/app/searchfor.sh
+++ fs/app/searchfor.sh
@@ -12,22 +12,25 @@ search="$1"
 outdir="$2"
 indir="$3"
 
-searchhash=$(base64 -w 0 <<<"$search")
-
-results=$outdir/$searchhash
-if [[ -e $results.processed ]] ; then
-    findargs="grep -v -F -f $results.processed"
-else
-    findargs=cat
-fi
-
 mydir=$(dirname $(readlink -f $0))
 
-# find the new domain files which we have not seen before
-find $indir -type f | $findargs | sort | while read f ; do
-    # for each newer file, crawl the results
-    python ${mydir}/hnrd.py -n -f "$f" -s "$search"
-    echo "$f" >>$results.processed
-done >>$results
+echo "# CSV files with DNS data" > README.md
+cat searches.txt | {
+    while read search; do
+        searchhash=$(base64 -w 0 <<<"$search")
 
-# TODO push $results and $results.processed back to repo
+        results=$outdir/$searchhash
+        if [[ -e $results.processed ]] ; then
+            findargs="grep -v -F -f $results.processed"
+        else
+            findargs=cat
+        fi
+
+    # find the new domain files which we have not seen before
+    find $indir -type f | $findargs | sort | while read f ; do
+        # for each newer file, crawl the results
+        python ${mydir}/hnrd.py -n -f "$f" -s "$search"
+        echo "$f" >>$results.processed
+    done >>$results
+    echo "- [`$search`]($searchhash)" >> README.md 
+done
