bb163cc635bd7a1ff7764746599fb1e862d27d2c
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-05-19 11:07:14 +0100

    Update plotgraph.sh

--- fs/app/plotgraph.sh
+++ fs/app/plotgraph.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 # script to graph results files
-# p1=search string
+# p1=file with line separate list of searches
 # p2=output directory
 # p3=input directory
 set -ex
-[[ -n $1 ]] || { echo "Search string not specified" >&2 ; exit 1 ; }
+[[ -n $1 ]] || { echo "Searches flile not specified" >&2 ; exit 1 ; }
 [[ -n $2 ]] || { echo "Output directory not specified" >&2 ; exit 1 ; }
 [[ -n $3 ]] || { echo "Input directory not specified" >&2 ; exit 1 ; }
 
-search="$1"
+searchesfile="$1"
 outdir="$2"
 indir="$3"
-
-searchhash=$(base64 -w 0 <<<"$search")
-
-results=$indir/$searchhash
-[[ -e $results ]] || { echo "No results available!" >&2 ; exit 1 ; }
 mydir=$(dirname $(readlink -f $0))
+readme=$outdir/README.md
+{
+    echo "# Graph plots for DNS registrations over time"
+} > $readme
 
-mkdir -p $outdir/$searchhash
-python3 $mydir/generate_graph.py $results $outdir/$searchhash "$search"
+{
+while read; do
+    searchhash=$(base64 -w 0 <<<"$REPLY")
+
+    results=$indir/$searchhash
+    [[ -e $results ]] || { echo "No results available!" >&2 ; exit 1 ; }
+
+    mkdir -p $outdir/$searchhash
+    python3 $mydir/generate_graph.py $results $outdir/$searchhash "$REPLY"
+    echo "- [\`$REPLY\`]($searchhash/index.html)"
+done
+} < $searchesfile
